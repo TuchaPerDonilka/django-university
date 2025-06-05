8f6e626a23e3b8cc5ac854e55381a5f49a8d4baa
TuchaPerDonilka <166588566+TuchaPerDonilka@users.noreply.github.com>
2025-06-05 12:54:01 +0700

    Update urls.py

--- example/urls.py
+++ example/urls.py
@@ -15,13 +15,11 @@ urlpatterns = [
     path('student/<int:pk>/edit/', StudentUpdateView.as_view(), name='student_update'),
     path('student/<int:pk>/delete/', StudentDeleteView.as_view(), name='student_delete'),
     
-    # Добавленные пути для предметов
     path('subjects/', SubjectListView.as_view(), name='subject_list'),
     path('subject/add/', SubjectCreateView.as_view(), name='subject_create'),
     path('subject/<int:pk>/edit/', SubjectUpdateView.as_view(), name='subject_update'),
     path('subject/<int:pk>/delete/', SubjectDeleteView.as_view(), name='subject_delete'),
     
-    # Добавленные пути для оценок
     path('grades/', GradeListView.as_view(), name='grade_list'),
     path('grade/add/', GradeCreateView.as_view(), name='grade_create'),
     path('grade/<int:pk>/edit/', GradeUpdateView.as_view(), name='grade_update'),
